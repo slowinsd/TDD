54e5b0655b016e7fc1a335d8313d8361b7655701
Dylan Slowinski <dylan.slowinski@hotmail.fr>
2016-04-22 11:31:39 +0200

    Add sources files

--- src/main/java/iut/tdd/RomanNumerals.java
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -3,15 +3,22 @@ package iut.tdd;
 public class RomanNumerals {
 	public String convertToRoman(String arabe) {
 		switch (arabe) {
-		case "5": return "V";
 		case"1"	: return "I";
+		case "5": return "V";
 		case "10":return "X";
 		case "50" :return "L";
 		case "100":return "C";
 		case "500":return "D";
 		case "1000" : return "M";
 		}
-		return "err";
+		
+		int value = Integer.parseInt(arabe);
+		StringBuilder romanNumber = new StringBuilder();
+		if(value==5) romanNumber=romanNumber.append("V");
+		for (int i = 0 ; i < value; i++) {
+			romanNumber.append("I");
+		}
+		return romanNumber.toString();
 	}
 
 	public String convertFromRoman(String roman) {
@@ -26,11 +33,12 @@ public class RomanNumerals {
 		}
 		return "err";
 	}
-	String[]romain=new String[]{"I","V","X","L"};
+	/*String[]romain=new String[]{"I","V","X","L"};
 	String[]chiffre=new String[]{"1","5","10","50"};
 	for(int i=0;i<roman.length;i++){
 		for(int j=0;i<romain.length;j++){
-			
+																																																																																																																																																																																																																																																																																																																																																																																																																												
 		}
-	}
+	}*/
 }
+
--- src/test/java/iut/tdd/RomanNumeralsTest.java
+++ src/test/java/iut/tdd/RomanNumeralsTest.java
@@ -7,9 +7,46 @@ public class RomanNumeralsTest {
 
 	@Test
 	public void should_return_I_when_1 () {
+		testConvertToRoman("1", "I");
+	}
+
+	@Test
+	public void should_return_V_when_5 () {
 		//Given
-		String input = "1";
-		String expected = "I";
+		testConvertToRoman("5", "V");
+	}
+	@Test
+	public void should_return_X_when_10 () {
+		//Given
+		testConvertToRoman("10", "X");
+	}
+	@Test
+	public void should_return_L_when_50 () {
+		//Given
+		testConvertToRoman("50", "L");
+	}
+	@Test
+	public void should_return_C_when_100 () {
+		//Given
+		testConvertToRoman("100", "C");
+	}
+	@Test
+	public void should_return_D_when_500 () {
+		//Given
+		testConvertToRoman("500", "D");
+	}
+	@Test
+	public void should_return_M_when_1000 () {
+		//Given
+		testConvertToRoman("1000", "M");
+	}
+	@Test
+	public void should_return_roman_number () {
+		testConvertToRoman("3", "III");
+		testConvertToRoman("6", "VI");
+	}
+	
+	private void testConvertToRoman(String input, String expected) {
 		RomanNumerals romanNumeral = new RomanNumerals();
 		//When
 		String actual = romanNumeral.convertToRoman(input);
@@ -17,70 +54,4 @@ public class RomanNumeralsTest {
 		Assert.assertEquals(expected, actual);
 	}
 
-	@Test
-	public void should_return_V_when_5 () {
-		//Given
-		String input = "5";
-		String expected = "V";
-		RomanNumerals romanNumeral = new RomanNumerals();
-		//When
-		String actual = romanNumeral.convertToRoman(input);
-		//Then
-		Assert.assertEquals(expected, actual);
-	}
-	@Test
-	public void should_return_X_when_10 () {
-		//Given
-		String input = "10";
-		String expected = "X";
-		RomanNumerals romanNumeral = new RomanNumerals();
-		//When
-		String actual = romanNumeral.convertToRoman(input);
-		//Then
-		Assert.assertEquals(expected, actual);
-	}
-	@Test
-	public void should_return_L_when_50 () {
-		//Given
-		String input = "50";
-		String expected = "L";
-		RomanNumerals romanNumeral = new RomanNumerals();
-		//When
-		String actual = romanNumeral.convertToRoman(input);
-		//Then
-		Assert.assertEquals(expected, actual);
-	}
-	@Test
-	public void should_return_C_when_100 () {
-		//Given
-		String input = "100";
-		String expected = "C";
-		RomanNumerals romanNumeral = new RomanNumerals();
-		//When
-		String actual = romanNumeral.convertToRoman(input);
-		//Then
-		Assert.assertEquals(expected, actual);
-	}
-	@Test
-	public void should_return_D_when_500 () {
-		//Given
-		String input = "500";
-		String expected = "D";
-		RomanNumerals romanNumeral = new RomanNumerals();
-		//When
-		String actual = romanNumeral.convertToRoman(input);
-		//Then
-		Assert.assertEquals(expected, actual);
-	}
-	@Test
-	public void should_return_M_when_1000 () {
-		//Given
-		String input = "1000";
-		String expected = "M";
-		RomanNumerals romanNumeral = new RomanNumerals();
-		//When
-		String actual = romanNumeral.convertToRoman(input);
-		//Then
-		Assert.assertEquals(expected, actual);
-	}
 }
